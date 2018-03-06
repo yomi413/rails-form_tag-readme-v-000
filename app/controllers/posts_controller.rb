4179f860e5508cfd9a1abf6bbcb934729e84d449
Yomaira Escano <yjescano@gmail.com>
2018-03-06 05:14:03 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to posts_path
+    redirect_to post_path
   end
 
 end
