f06ae2dfccba311aaa8eceb6c949f2ec3e5c3034
Yomaira Escano <yjescano@gmail.com>
2018-03-06 05:16:08 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -18,7 +18,7 @@ describe 'new post' do
 
     click_on 'Submit Post'
 
-    expect(page.current_path).to eq(post_path)
+    expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
   end
