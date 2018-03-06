61828cc5509e246631ff35a17131ce2146486f20
Yomaira Escano <yjescano@gmail.com>
2018-03-06 04:46:40 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,4 +5,9 @@ describe 'new post' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
 end
